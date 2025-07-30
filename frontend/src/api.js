776206eca33711d5fe2fd818f54c97238b71f057
anshu272003 <139258380+anshu272003@users.noreply.github.com>
2025-07-30 11:20:05 +0530

    last

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL, // for Vite
+  // baseURL: process.env.REACT_APP_API_BASE_URL, // if using Create React App
 });
+
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
+
 export default API;
